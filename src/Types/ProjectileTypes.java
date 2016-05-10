e4636c50ba430b1478b8be9e3878cfe590987990
unknown <plevinskyep16@LHS-174-8.world.sau12>
2016-05-10 13:58:23 -0400

    Something went wrong

--- src/Types/ProjectileTypes.java
+++ src/Types/ProjectileTypes.java
@@ -7,7 +7,7 @@ import Math.Vector2D;
 public class ProjectileTypes {
 	//                                                 new ProjectileInfoComponent(border, target, speed, accuracy, range, AoE, AoEDamage, slow, DoT flytime, damage, image);
 	
-	public static ProjectileInfoComponent Cannonball = new ProjectileInfoComponent(new Shape(16), new Vector2D(0,0), 2f, 5f, 2f, 75f, .25f, 0, 0, 0, 100,  "/Images/car.png");
-	public static ProjectileInfoComponent HeavyCannonball = new ProjectileInfoComponent(new Shape(16), new Vector2D(0,0), 1f, 0f, 1f, 125f, .75f, 0, 0, 0, 200,  "/Images/car.png");
-	public static ProjectileInfoComponent PirateArcher = new ProjectileInfoComponent(new Shape(16), new Vector2D(0,0), 3f, 0f, .5f, 0, 0, 0, 0, 0, 50,  "/Images/car.png");
+	public static ProjectileInfoComponent Cannonball = new ProjectileInfoComponent(new Shape(16), new Vector2D(0,0), 2f, 5f, 2f, 75f, .25f, 0, 0, 0, 100,  "/Images/Pirateship.png");
+	public static ProjectileInfoComponent HeavyCannonball = new ProjectileInfoComponent(new Shape(16), new Vector2D(0,0), 1f, 0f, 1f, 125f, .75f, 0, 0, 0, 200,  "/Images/Pirateship.png");
+	public static ProjectileInfoComponent PirateArcher = new ProjectileInfoComponent(new Shape(16), new Vector2D(0,0), 3f, 0f, .5f, 0, 0, 0, 0, 0, 50,  "/Images/Pirateship.png");
 }
